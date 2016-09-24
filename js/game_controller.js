4fc187ebc0d4c6fc4636d6f1d693c9d9c09de905
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-24 17:12:00 -0500

    Stdin still doesn't work

--- js/game_controller.js
+++ js/game_controller.js
@@ -23,3 +23,22 @@ function GameController() {
   };
 
 };
+
+// process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+
+var text = process.stdin.on('data', function (text) {
+  console.log('received data:', util.inspect(text));
+  // if (text === 'quit\n') {
+    done();
+  // }
+});
+
+function done() {
+  console.log('Now that process.stdin is paused, there is nothing more to do.');
+  process.exit();
+}
+
+// console.log(text)
+
