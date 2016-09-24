75a178ed3461c0d054708182525738657382b803
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-24 14:16:33 -0500

    Refactor to constructor

--- js/solution_generator.js
+++ js/solution_generator.js
@@ -1,12 +1,15 @@
 // solution generator
 
-function generateSolution() {
-  var colors = ["yellow", "blue", "green", "red", "purple", "orange"];
-  var solution = [];
+function SolutionGenerator() {
+  this.generate = function() {
+    var colors = ["yellow", "blue", "green", "red", "purple", "orange"];
+    var solution = [];
 
-  while (solution.length < 4) {
-    var pick = Math.floor(Math.random() * colors.length);
-    solution.push(colors.splice(pick, 1)[0]);
-  }
-  return solution
-}
+    while (solution.length < 4) {
+      var pick = Math.floor(Math.random() * colors.length);
+      solution.push(colors.splice(pick, 1)[0]);
+    }
+
+    return solution;
+  };
+};
