54e6f8609dbb144408fe4d15de8904579bbe80d0
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-24 16:06:49 -0500

    Add solved? function

--- js/game.js
+++ js/game.js
@@ -1,7 +1,14 @@
 // game model
 
 function Game(solution) {
-  var solution: solution;
+  var solution = solution;
   this.getSolution = function() { return solution };
-  this.guesses: {};
+  this.guesses = {};
+
+  this.solved? = function() {
+    if (Object.keys(guesses)[guesses.length - 1] == solution) {
+      return true;
+    };
+    return false;
+  };
 };
